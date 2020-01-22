3d979e6a6881dc73a9ea6ca96c610bc24827e0d4
PetrasSip <petras.sipavicius@gmail.com>
2020-01-22 20:42:57 +0200

    rout to PagesController added

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+Route::get('/', 'PagesController@index')->name('dashboard');
 
 Auth::routes();
 
