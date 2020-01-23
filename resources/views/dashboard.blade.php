cadc95adff3c99bdd4d31334a903bf7dc86d0d58
PetrasSip <petras.sipavicius@gmail.com>
2020-01-23 21:50:42 +0200

    view blades changed

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <div class="row justify-content-center">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"><h1>PRODUCTS</h1></div>
+                <div class="card-body">
+                    <div class="row">
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
