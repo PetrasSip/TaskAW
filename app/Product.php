955b59169ac281aab3b333bf2d1b7334f02eab11
PetrasSip <petras.sipavicius@gmail.com>
2020-01-23 18:38:10 +0200

    model relationships added

--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,34 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    public function setting()
+    {
+        return $this->hasOne('App\Setting');
+    }
+
+
+    public function productDiscount()
+    {
+        return $this->hasOne('App\ProductDiscount');
+    }
+
+
+    public function quantity()
+    {
+        return $this->hasOne('App\Quantity');
+    }
+
+
+    public function productReviews()
+    {
+        return $this->hasMany('App\ProductReview');
+    }
+
+
+    public function productSpecifications()
+    {
+        return $this->hasMany('App\ProductReview');
+    }
+
+
 }
--- app/Setting.php
+++ app/Setting.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Setting extends Model
 {
-    //
+    public function product()
+    {
+        return $this->belongsTo('App\Product');
+    }
 }
